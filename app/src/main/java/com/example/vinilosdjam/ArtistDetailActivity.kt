57c9e008129b54b03dd0fca8b60ccba05f4261b1
Manuel Sanchez Masferrer <ma.sanchezm12@uniandes.edu.co>
2021-11-21 12:56:46 -0600

    ÂFragments added for artist

---
+++ app/src/main/java/com/example/vinilosdjam/ArtistDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vinilosdjam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtistDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artist_detail)
+    }
+}
