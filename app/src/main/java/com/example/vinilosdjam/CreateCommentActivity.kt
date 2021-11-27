f8ab6ce489cebb0999009e8b22bfef807a1fab51
jorgep <je.pacheco@uniandes.edu.co>
2021-11-26 21:59:51 -0500

    Creación de actividades para HU07 Y HU08

---
+++ app/src/main/java/com/example/vinilosdjam/CreateCommentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vinilosdjam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateCommentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_comment)
+    }
+}
