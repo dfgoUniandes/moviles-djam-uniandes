da8fc16e62e75c9355b5aa610094cbc245fc7c89
Manuel Sanchez Masferrer <ma.sanchezm12@uniandes.edu.co>
2021-12-05 17:24:21 -0600

    fixed accessibility issues

--- app/src/main/java/com/example/vinilosdjam/database/ArtistDao.kt
+++ app/src/main/java/com/example/vinilosdjam/database/ArtistDao.kt
@@ -1,8 +1,6 @@
 package com.example.vinilosdjam.database
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.vinilosdjam.models.Artist
 
@@ -10,10 +8,10 @@ import com.example.vinilosdjam.models.Artist
 interface ArtistDao {
     @Query("SELECT * FROM artists_table")
     fun getArtist():List<Artist>
+}
 
 //    @Insert(onConflict = OnConflictStrategy.IGNORE)
 //    suspend fun insert(artist: Artist)
 //
 //    @Query("DELETE FROM artists_table")
 //    suspend fun deleteAll():Int
-}
